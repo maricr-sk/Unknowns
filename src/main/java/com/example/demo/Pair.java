5f3f967fb2cd4b7dba45af061340b306d4278df8
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-01-03 18:31:24 -0500

    initial commit

---
+++ src/main/java/com/example/demo/Pair.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javafx.application.Application.launch;
+
+public class Pair Project extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
